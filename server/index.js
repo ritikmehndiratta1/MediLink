6ab4ff7ea9cc5e3ab0ec7513e08b08b2faacbf8f
ritikmehndiratta1 <52930602+ritikmehndiratta1@users.noreply.github.com>
2026-07-13 00:18:49 +0530

    setup2

--- server/index.js
+++ server/index.js
@@ -1,13 +1,39 @@
+require("dotenv").config();
+
 const express = require("express");
+const { Pool } = require("pg");
 
 const app = express();
 
 const PORT = process.env.PORT || 5000;
 
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
+
 app.get("/", (req, res) => {
-    res.send("Medilink API is running 🚀");
+  res.send("🚀 Medilink API is running!");
+});
+
+app.get("/test-db", async (req, res) => {
+  try {
+    const result = await pool.query("SELECT NOW()");
+    res.json({
+      success: true,
+      serverTime: result.rows[0].now,
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({
+      success: false,
+      error: err.message,
+    });
+  }
 });
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
